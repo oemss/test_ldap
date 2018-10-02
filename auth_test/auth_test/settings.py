9be71340dc47f753abe438b911dbb9498aab0cab
oemss <thispagenotfound@yandex.ru>
2018-10-02 22:21:42 +0300

    add AUTH

--- auth_test/auth_test/settings.py
+++ auth_test/auth_test/settings.py
@@ -11,80 +11,15 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import ldap
-from django_auth_ldap.config import LDAPSearch, GroupOfNamesType
 
 
-# Baseline configuration.
-AUTH_LDAP_SERVER_URI = 'ldap://ldap.example.com'
-
-AUTH_LDAP_BIND_DN = 'cn=django-agent,dc=example,dc=com'
-AUTH_LDAP_BIND_PASSWORD = 'phlebotinum'
-AUTH_LDAP_USER_SEARCH = LDAPSearch(
-    'ou=users,dc=example,dc=com',
-    ldap.SCOPE_SUBTREE,
-    '(uid=%(user)s)',
-)
-# Or:
-# AUTH_LDAP_USER_DN_TEMPLATE = 'uid=%(user)s,ou=users,dc=example,dc=com'
-
-# Set up the basic group parameters.
-AUTH_LDAP_GROUP_SEARCH = LDAPSearch(
-    'ou=django,ou=groups,dc=example,dc=com',
-    ldap.SCOPE_SUBTREE,
-    '(objectClass=groupOfNames)',
-)
-AUTH_LDAP_GROUP_TYPE = GroupOfNamesType(name_attr='cn')
-
-# Simple group restrictions
-AUTH_LDAP_REQUIRE_GROUP = 'cn=enabled,ou=django,ou=groups,dc=example,dc=com'
-AUTH_LDAP_DENY_GROUP = 'cn=disabled,ou=django,ou=groups,dc=example,dc=com'
-
-# Populate the Django user from the LDAP directory.
-AUTH_LDAP_USER_ATTR_MAP = {
-    'first_name': 'givenName',
-    'last_name': 'sn',
-    'email': 'mail',
-}
-
-AUTH_LDAP_USER_FLAGS_BY_GROUP = {
-    'is_active': 'cn=active,ou=django,ou=groups,dc=example,dc=com',
-    'is_staff': 'cn=staff,ou=django,ou=groups,dc=example,dc=com',
-    'is_superuser': 'cn=superuser,ou=django,ou=groups,dc=example,dc=com',
-}
-
-# This is the default, but I like to be explicit.
-AUTH_LDAP_ALWAYS_UPDATE_USER = True
-
-# Use LDAP group membership to calculate group permissions.
-AUTH_LDAP_FIND_GROUP_PERMS = True
-
-# Cache distinguised names and group memberships for an hour to minimize
-# LDAP traffic.
-AUTH_LDAP_CACHE_TIMEOUT = 3600
-
-# Keep ModelBackend around for per-user permissions and maybe a local
-# superuser.
-AUTHENTICATION_BACKENDS = (
+AUTHENTICATION_BACKENDS = [
     'django_auth_ldap.backend.LDAPBackend',
-    'django.contrib.auth.backends.ModelBackend',
-)
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+]
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+AUTH_LDAP_SERVER_URI = "ldap://127.0.0.1:389"
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xynohdq#j3#p)ls=#+wi7=%gyq*27y!3c$czxu7#^sh1z^cm3t'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -125,16 +60,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'auth_test.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
